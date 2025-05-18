c5b46b92c1113881d75bfbd4c787229e0c742120
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-05-18 16:24:52 +0530

    done

--- src/components/FilterDropdown.jsx
+++ src/components/FilterDropdown.jsx
@@ -1,11 +1,17 @@
+//FilterDropdown
+
 const FilterDropdown = ({ setRegion, value }) => (
   <select
+    
     className="form-select"
     value={value}
     onChange={(e) => setRegion(e.target.value)}
     style={{ maxWidth: '300px' }}
   >
+  
     <option value="">All Regions</option>
+
+    {/* Region-specific options */}
     <option value="Africa">Africa</option>
     <option value="Americas">Americas</option>
     <option value="Asia">Asia</option>
