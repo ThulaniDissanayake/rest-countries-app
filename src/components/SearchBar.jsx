eadc3408cc7a1369a6cf7c25273f0c60d7c08971
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-05-18 16:22:57 +0530

    done

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,12 +1,25 @@
-const SearchBar = ({ setSearch, value }) => (
-  <input
-    type="text"
-    className="form-control"
-    placeholder="Search by name"
-    value={value}
-    onChange={(e) => setSearch(e.target.value)}
-    style={{ maxWidth: '250px' }}
-  />
+const SearchBar = ({ setSearch, setSearchCode, search, searchCode }) => (
+  <div className="d-flex flex-column flex-md-row gap-3 mb-3">
+    {/* Search by Country Name */}
+    <input
+      type="text"
+      className="form-control"
+      placeholder="Search by country name"
+      value={search}
+      onChange={(e) => setSearch(e.target.value)}
+      style={{ maxWidth: '250px' }}
+    />
+
+    {/* Search by Country Code */}
+    <input
+      type="text"
+      className="form-control"
+      placeholder="Search by country code (eg.US)"
+      value={searchCode}
+      onChange={(e) => setSearchCode(e.target.value.toUpperCase())}  
+      style={{ maxWidth: '250px' }}
+    />
+  </div>
 );
 
 export default SearchBar;
