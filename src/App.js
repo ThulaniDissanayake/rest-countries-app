a54fe961ded040369981b1a86bd8d96b2376300b
ThulaniDissanayake <IT22157232@my.sliit.lk>
2025-05-18 16:26:35 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,46 @@
-import React, { useState} from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AuthProvider, useAuth } from './context/AuthContext';
+import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import CountryDetails from './pages/CountryDetails';
 import Login from './components/Login';
+import Register from './components/Register';
 
-const App = () => {
-  const [user, setUser] = useState(localStorage.getItem('user'));
+const AppContent = () => {
+  const { user, logout } = useAuth();
 
   return (
     <Router>
-      {!user ? (
-        <Login setUser={setUser} />
-      ) : (
+      {user ? (
         <>
-          <div className="p-4 text-right">Welcome, {user}</div>
+          {/* Navbar shown when user is logged in, with user and logout props */}
+          <Navbar user={user} logout={logout} />
+
+          {/* Protected routes */}
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/country/:code" element={<CountryDetails />} />
+          </Routes>
+        </>
+      ) : (
+        <>
+          {/* Public routes */}
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="*" element={<Login />} />
           </Routes>
         </>
       )}
     </Router>
   );
 };
+
+const App = () => (
+  <AuthProvider>
+    <AppContent />
+  </AuthProvider>
+);
 
 export default App;
